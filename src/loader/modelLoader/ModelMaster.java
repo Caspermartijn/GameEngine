9ac62604e39f9a284f67061a35209980345c3b12
gebruiker-pc <gebruiker-pc@gebruiker.home>
2018-01-01 23:14:26 +0100

    Updates

--- src/loader/modelLoader/ModelMaster.java
+++ src/loader/modelLoader/ModelMaster.java
@@ -1,12 +1,15 @@
 package loader.modelLoader;
 
+import java.io.InputStream;
 import java.util.HashMap;
 
 public class ModelMaster {
 
 	private static HashMap<String, ModelData> models = new HashMap<String, ModelData>();
 
-	public static void loadModels(String modelsPath){
+	public static void loadModels(String modelsPath) {
+		InputStream inputStream = Class.class.getResourceAsStream("modelsPath");
+
 	}
-	
+
 }
