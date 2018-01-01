2082aeb665ddc5aeadccf83aa3c34c012502a634
gebruiker-pc <gebruiker-pc@gebruiker.home>
2018-01-01 21:48:02 +0100

    push

--- src/loader/modelLoader/ModelMaster.java
+++ src/loader/modelLoader/ModelMaster.java
@@ -1,5 +1,12 @@
 package loader.modelLoader;
 
+import java.util.HashMap;
+
 public class ModelMaster {
 
+	private static HashMap<String, ModelData> models = new HashMap<String, ModelData>();
+
+	public static void loadModels(String modelsPath){
+	}
+	
 }
