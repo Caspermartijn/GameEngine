6919f4a14451f0c88132de3b4e16bc3cbf7d60a0
Florian <Florian@DESKTOP-JKQTN2Q.fritz.box>
2018-01-01 21:20:46 +0100

    FontRenderer fixes

--- src/renderer/textRendering/FontRenderer.java
+++ src/renderer/textRendering/FontRenderer.java
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.GL11;
 
 import engine.GLSettings;
 import objects.Vao;
+import texts.Fonts;
 import texts.Text;
 
 public class FontRenderer {
@@ -22,10 +23,10 @@ public class FontRenderer {
 		Vao vao = text.getMesh();
 		vao.bind(0, 1);
 		
-		shader.texture.bindTexture(text.getFont().getTexture());
+		shader.texture.bindTexture(Fonts.getFont(text.getFont()).getTexture());
 		shader.color.loadVec4(text.getColor());
 		shader.translation.loadVec2(text.getPosition());
-		shader.fontSettings.loadVec2(text.getFont().getWidth(), text.getFont().getSmoothness());
+		shader.fontSettings.loadVec2(Fonts.getFont(text.getFont()).getWidth(), Fonts.getFont(text.getFont()).getSmoothness());
 		
 		GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, text.getVertexCount());
 		
