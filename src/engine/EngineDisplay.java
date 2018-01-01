b54accf26642087ceb87bf21fdd0adb0c8de4fbf
gebruiker-pc <gebruiker-pc@gebruiker.home>
2018-01-01 15:45:27 +0100

    Update

--- src/engine/EngineDisplay.java
+++ src/engine/EngineDisplay.java
@@ -1,5 +1,33 @@
 package engine;
 
 public class EngineDisplay {
+
+	private DisplaySettings displaySettings;
+
+	public EngineDisplay(DisplaySettings displaySettings) {
+		this.displaySettings = displaySettings;
+	}
 	
+	public void create(){
+		
+		
+		
+	}
+	
+	public void close(){
+		
+	}
+
+	private void updateFPS(){
+		
+	}
+	
+	public DisplaySettings getDisplaySettings() {
+		return displaySettings;
+	}
+
+	public void setDisplaySettings(DisplaySettings displaySettings) {
+		this.displaySettings = displaySettings;
+	}
+
 }
