9bd7358e51b40d090fb9b7041316cef9c1bc30fe
gebruiker-pc <gebruiker-pc@gebruiker.home>
2018-01-02 15:41:55 +0100

    Matrix fix

--- src/shaders/shaderObjects/UniformMat4.java
+++ src/shaders/shaderObjects/UniformMat4.java
@@ -20,6 +20,7 @@ public class UniformMat4 extends Uniform {
 		Matrix.store(matrix, matrixBuffer);
 		matrixBuffer.flip();
 		GL20.glUniformMatrix4fv(getLocation(), false, matrixBuffer);
+		matrixBuffer.clear();
 	}
 
 }
