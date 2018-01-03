7f5708562e84a061b7008bde74625a805a8d8416
gebruiker-pc <gebruiker-pc@192.168.2.9>
2018-01-03 16:43:14 +0100

    push

--- engineTest/main/EngineTester.java
+++ engineTest/main/EngineTester.java
@@ -55,7 +55,7 @@ public class EngineTester {
 
 			@Override
 			public void quit() {
-
+				this.closeApp();
 			}
 
 		};
--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.List;
+
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -13,6 +15,10 @@ public class Entity implements IEntity {
 	private float scale;
 
 	private Model_3D model;
+	
+	private Entity parent;
+	
+	private List<Entity> children;
 
 	public Entity(Model_3D model, Vector3f position, Vector3f rotation, float scale) {
 		this.position = position;
@@ -21,6 +27,19 @@ public class Entity implements IEntity {
 		this.model = model;
 	}
 
+	public void addChild(Entity entity) {
+		children.add(entity);
+		entity.setParent(this);
+	}
+	
+	protected void setParent(Entity entity) {
+		this.parent = entity;
+	}
+
+	public Entity getParent() {
+		return parent;
+	}
+	
 	@Override
 	public void update() {
 
@@ -50,8 +69,27 @@ public class Entity implements IEntity {
 		return model;
 	}
 
+	private Matrix4f generateMatrix() {
+			if(parent == null) {
+				Matrix4f matrix = new Matrix4f();
+				matrix.setIdentity();
+				return matrix;
+			}else {
+				return parent.getParentMatrix();
+			}
+	}
+	
+	private Matrix4f getParentMatrix() {
+		Matrix4f matrix = generateMatrix();
+		Matrix4f.rotate((float) Math.toRadians(rotation.x), new Vector3f(1, 0, 0), matrix, matrix);
+		Matrix4f.rotate((float) Math.toRadians(rotation.y), new Vector3f(0, 1, 0), matrix, matrix);
+		Matrix4f.rotate((float) Math.toRadians(rotation.z), new Vector3f(0, 0, 1), matrix, matrix);
+		Matrix4f.translate(position, matrix, matrix);
+		return matrix;
+	}
+
 	public Matrix4f getTransformationMatrix() {
-		Matrix4f matrix = new Matrix4f();
+		Matrix4f matrix = generateMatrix();
 		matrix.setIdentity();
 		Matrix4f.scale(new Vector3f(scale, scale, scale), matrix, matrix);
 		Matrix4f.rotate((float) Math.toRadians(rotation.x), new Vector3f(1, 0, 0), matrix, matrix);
