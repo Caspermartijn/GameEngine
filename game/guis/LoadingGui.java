650b219bb795960a017fdb59fc517f2902ab41ee
gebruiker-pc <gebruiker-pc@gebruiker.home>
2018-01-13 20:19:36 +0100

    Push

--- game/guis/LoadingGui.java
+++ game/guis/LoadingGui.java
@@ -3,6 +3,7 @@ package guis;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector2f;
 
+import engine.Display;
 import utils.SourceFile;
 import utils.maths.Maths;
 
@@ -12,30 +13,30 @@ public class LoadingGui extends GUI {
 		images();
 	}
 
+	private ImageComponent hourglass;
+
 	private void images() {
 
-		ImageComponent background = new ImageComponent(this, new SourceFile("/res/guis/loading/background.png"));
+		ImageComponent background = new ImageComponent(this, new SourceFile("/res/guis/loading/background_2.png"));
 		Vector2f size = Maths.getFrom720toCurrentDisplaySize(new Vector2f(1280, 720));
 		background.setSize(new Vector2f(size.x, size.y));
 		background.setPosition(0.5f, 0.5f);
-		background.show();
 
-		ImageComponent hourglass = new ImageComponent(this, new SourceFile("/res/guis/loading/hourglass.png"));
-		Vector2f size_1 = Maths.getFrom720toCurrentDisplaySize(new Vector2f(75, 75));
+		hourglass = new ImageComponent(this, new SourceFile("/res/guis/loading/hourglass.png"));
+		Vector2f size_1 = Maths.getFrom720toCurrentDisplaySize(new Vector2f(37.5f, 37.5f));
 		hourglass.setSize(new Vector2f(size_1.x, size_1.y));
 		hourglass.setPosition(0.90f, 0.85f);
-		hourglass.show();
 
-		ImageComponent circle_1 = new ImageComponent(this, new SourceFile("/res/guis/loading/ring_1.png"));
-		Vector2f size_2 = Maths.getFrom720toCurrentDisplaySize(new Vector2f(110, 120));
-		circle_1.setSize(new Vector2f(size_2.x, size_2.y));
-		circle_1.setPosition(0.90f, 0.85f);
-		circle_1.show();
+		ImageComponent circle = new ImageComponent(this, new SourceFile("/res/guis/loading/ring_1.png"));
+		Vector2f size_3 = Maths.getFrom720toCurrentDisplaySize(new Vector2f(95, 95));
+		circle.setSize(new Vector2f(size_3.x, size_3.y));
+		circle.setPosition(0.90f, 0.85f);
 
 	}
 
 	public void renderComps() {
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		hourglass.setRotation((float) (hourglass.getRotation() + 180 * Display.getFrameTime()));
 		super.renderComponents();
 	}
 
