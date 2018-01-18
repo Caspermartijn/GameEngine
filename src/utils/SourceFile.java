81a1c6a0ab51fb1f7cfd36d4ca9fad48bbec13d7
gebruiker-pc <gebruiker-pc@192.168.2.9>
2018-01-18 14:53:12 +0100

    push

--- src/utils/SourceFile.java
+++ src/utils/SourceFile.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class SourceFile {
