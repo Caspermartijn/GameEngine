198631ebde623b4bd72d21e753872e49ace99574
gebruiker-pc <gebruiker-pc@gebruiker.home>
2018-01-26 10:39:39 +0100

    Fixed some unrealistic player movements

--- game/objects/FPS_Player.java
+++ game/objects/FPS_Player.java
@@ -20,10 +20,12 @@ public class FPS_Player extends Camera {
 	private static final float MOUSE_SENSITY = 100;
 	private static final float SPEED = 20;
 
-	public static final float GRAFITY = -7.5f;
-	private static final float JUMP_POWER = 1.3f;
+	public static final float GRAFITY = -5.50f;
+	private static final float JUMP_POWER = 1.50f;
 
 	private FPS_HUD hud;
+
+	private float playerHeight = 5;
 
 	public FPS_Player(Vector3f start_position) {
 		super.x = start_position.x;
@@ -54,6 +56,8 @@ public class FPS_Player extends Camera {
 		projection.m33 = 0;
 	}
 
+	private float startXincr = 0, startZincr = 0, startXincr2 = 0, startZincr2 = 0;
+
 	public void updateInputs() {
 		float dx = (float) (Mouse.getMouseDX() * MOUSE_SENSITY);
 		float dy = (float) (Mouse.getMouseDY() * MOUSE_SENSITY);
@@ -61,30 +65,59 @@ public class FPS_Player extends Camera {
 		super.pitch += dy;
 		pitch = Maths.clamp(-70, 80, pitch);
 
-		float xIncr = 0;
-		float zIncr = 0;
+		float xIncr = startXincr;
+		float zIncr = startZincr;
 
-		float xIncr2 = 0;
-		float zIncr2 = 0;
+		float xIncr2 = startXincr2;
+		float zIncr2 = startZincr2;
 
-		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_W)) {
-			xIncr += Math.sin(Math.toRadians(yaw)) * Display.getFrameTime() * SPEED;
-			zIncr -= Math.cos(Math.toRadians(yaw)) * Display.getFrameTime() * SPEED;
+		float actual_speed = SPEED;
+
+		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_SHIFT)) {
+			if (!isInAir) {
+				actual_speed = SPEED * 1.75f;
+				if (crouched) {
+					undoCrouch();
+				}
+			}
 		}
-		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_S)) {
-			xIncr += Math.sin(Math.toRadians(yaw)) * Display.getFrameTime() * -SPEED;
-			zIncr -= Math.cos(Math.toRadians(yaw)) * Display.getFrameTime() * -SPEED;
+
+		if (!isInAir) {
+			if (Keyboard.isKeyDown(GLFW.GLFW_KEY_W)) {
+				xIncr += Math.sin(Math.toRadians(yaw)) * Display.getFrameTime() * actual_speed;
+				zIncr -= Math.cos(Math.toRadians(yaw)) * Display.getFrameTime() * actual_speed;
+
+			}
+			if (Keyboard.isKeyDown(GLFW.GLFW_KEY_S)) {
+				xIncr += Math.sin(Math.toRadians(yaw)) * Display.getFrameTime() * -actual_speed;
+				zIncr -= Math.cos(Math.toRadians(yaw)) * Display.getFrameTime() * -actual_speed;
+
+			}
+			if (Keyboard.isKeyDown(GLFW.GLFW_KEY_A)) {
+				xIncr2 -= Math.sin(Math.toRadians(yaw + 90)) * Display.getFrameTime() * SPEED;
+				zIncr2 += Math.cos(Math.toRadians(yaw + 90)) * Display.getFrameTime() * SPEED;
+			}
+			if (Keyboard.isKeyDown(GLFW.GLFW_KEY_D)) {
+				xIncr2 -= Math.sin(Math.toRadians(yaw + 90)) * Display.getFrameTime() * -SPEED;
+				zIncr2 += Math.cos(Math.toRadians(yaw + 90)) * Display.getFrameTime() * -SPEED;
+			}
 		}
-		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_A)) {
-			xIncr2 -= Math.sin(Math.toRadians(yaw + 90)) * Display.getFrameTime() * SPEED;
-			zIncr2 += Math.cos(Math.toRadians(yaw + 90)) * Display.getFrameTime() * SPEED;
-		}
-		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_D)) {
-			xIncr2 -= Math.sin(Math.toRadians(yaw + 90)) * Display.getFrameTime() * -SPEED;
-			zIncr2 += Math.cos(Math.toRadians(yaw + 90)) * Display.getFrameTime() * -SPEED;
-		}
+
 		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_SPACE)) {
 			jump();
+			setStartIncr(xIncr, zIncr, xIncr2, zIncr2);
+		}
+		if (Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_CONTROL)) {
+			if (!crouchedButtonPress) {
+				crouchedButtonPress = true;
+				if (!crouched) {
+					crouch();
+				} else {
+					undoCrouch();
+				}
+			}
+		} else {
+			crouchedButtonPress = false;
 		}
 
 		super.x += xIncr;
@@ -97,16 +130,53 @@ public class FPS_Player extends Camera {
 		super.y += upSpeed;
 
 		float terrainH = Scene.getCurrentscene().getTerrains().get(0).getHeightOfTerrain(this.x, this.z);
-		if (super.y < terrainH + 5) {
+
+		if (super.y < terrainH + playerHeight) {
 			upSpeed = 0;
 			isInAir = false;
-			super.y = terrainH + 5;
+			super.y = terrainH + playerHeight;
+			resetStartVelocity();
+		}
+
+		if (crouched) {
+			playerHeight = 2.5f;
+		} else {
+			playerHeight = 5;
 		}
 
 		super.updateMatrix();
 	}
 
+	private void setStartIncr(float xIncr, float zIncr, float xIncr2, float zIncr2) {
+		startXincr = xIncr;
+		startXincr2 = xIncr2;
+		startZincr = zIncr;
+		startZincr2 = zIncr2;
+	}
+
+	private void resetStartVelocity() {
+		startXincr = 0;
+		startXincr2 = 0;
+		startZincr = 0;
+		startZincr2 = 0;
+	}
+
+	private boolean crouched = false;
+	private boolean crouchedButtonPress = false;
+
+	private void crouch() {
+		crouched = true;
+	}
+
+	private void undoCrouch() {
+		crouched = false;
+		playerHeight = 3.25f;
+	}
+
 	private void jump() {
+		if (crouched) {
+			undoCrouch();
+		}
 		if (!isInAir) {
 			this.upSpeed = JUMP_POWER;
 			isInAir = true;
