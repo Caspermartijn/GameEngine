95479141c490e4127398d4e78ef90cdbbfe8ee25
Florian <Florian@DESKTOP-JKQTN2Q.fritz.box>
2018-02-04 13:48:59 +0100

    animations completed

--- src/animation/Joint.java
+++ src/animation/Joint.java
@@ -6,17 +6,17 @@ import java.util.List;
 import org.lwjgl.util.vector.Matrix4f;
 
 public class Joint {
-	
+
 	public static final int MAX_WEIGHTS = 3;
 	public static final int MAX_JOINTS = 50;
-	
+
 	public final int index;
 	public final String name;
 	public final String parentName;
 	public final List<Joint> children = new ArrayList<Joint>();
 
 	private Matrix4f transform;
-	
+
 	public final Matrix4f localBindTransform;
 	private Matrix4f inverseBindTransform = new Matrix4f();
 
@@ -25,7 +25,7 @@ public class Joint {
 		this.name = name;
 		this.parentName = parentName;
 		this.localBindTransform = localBindTransform;
-		transform = localBindTransform;
+		transform = new Matrix4f();
 	}
 
 	public void addChild(Joint child) {
@@ -39,12 +39,13 @@ public class Joint {
 			child.calcInverseBindTransform(bindTransform);
 		}
 	}
-	
+
 	public void calculateTotalTransform(Matrix4f[] matrices, Matrix4f parentTransform) {
-		Matrix4f currentTransform = new Matrix4f(transform);
+		Matrix4f currentTransform = new Matrix4f();
+		Matrix4f.mul(localBindTransform, transform, currentTransform);
 		if (parentTransform != null) {
 			Matrix4f.mul(parentTransform, currentTransform, currentTransform);
-		} 
+		}
 		for (Joint j : children) {
 			j.calculateTotalTransform(matrices, currentTransform);
 		}
@@ -56,7 +57,7 @@ public class Joint {
 	}
 
 	public void setTransform(Matrix4f transform) {
-		Matrix4f.mul(localBindTransform, transform, this.transform);
+		this.transform = transform;
 	}
 
 	public int getID() {
@@ -72,7 +73,7 @@ public class Joint {
 		for (Joint child : children) {
 			child.load(transforms);
 		}
+
 	}
-	
-	
+
 }
